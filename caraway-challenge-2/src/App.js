1918351a05cac89c2804bf4d5ade043adcae9f0a
joelgonzadev <joel@alliofintech.io>
2022-06-13 16:03:09 -0500

    First draft

---
+++ caraway-challenge-2/src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import Card from "./components/Card";
+import styled from "@emotion/styled";
+import ShowerBG from "./assets/shower-bg.jpg";
+import Shower from "./assets/shower-product.png";
+import FoamBG from "./assets/foam-bg.jpg";
+import Foam from "./assets/foam-product.png";
+import FluffyBG from "./assets/fluffy-bg.jpg";
+import Fluffy from "./assets/fluffy-product.png";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-color: #f1f0ef;
+`;
+
+const products = [
+  {
+    title: "Shower Foam",
+    subTitle: "Motivation and focus*",
+    info: "Warm Sugar, Vanilla, Fresh Pancakes",
+    backgroundImage: ShowerBG,
+    productImage: Shower,
+    regPrice: "$50.00",
+    subPrice: "42.50",
+    bgColor: "#eeea8f",
+  },
+  {
+    title: "Sea Foam",
+    subTitle: "Connection and joy*",
+    info: "Sea Salt, Beach Water, Fresh Coconut, Sprinkles",
+    backgroundImage: FoamBG,
+    productImage: Foam,
+    regPrice: "$50.00",
+    subPrice: "42.50",
+    bgColor: "#b0eaeb",
+  },
+  {
+    title: "Fluffy Foam",
+    subTitle: "Deep, restorative sleep*",
+    info: "Fresh Berries, Rainbow Sprinkles, Fairy Floss",
+    backgroundImage: FluffyBG,
+    productImage: Fluffy,
+    regPrice: "$50.00",
+    subPrice: "42.50",
+    bgColor: "#ffc0b2",
+  },
+];
+
+function App() {
+  return (
+    <Container>
+      {products.map((product) => (
+        <Card
+          title={product.title}
+          subTitle={product.subTitle}
+          info={product.info}
+          backgroundImage={product.backgroundImage}
+          productImage={product.productImage}
+          buttonColor={product.bgColor}
+          regPrice={product.regPrice}
+          subPrice={product.subPrice}
+        />
+      ))}
+    </Container>
+  );
+}
+
+export default App;
---
+++ caraway-challenge-2/src/components/Card.js
@@ -0,0 +1,127 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div(() => ({
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(12rem, 16rem))",
+  gap: "10px",
+  justifyContent: "center",
+  margin: "40px",
+}));
+
+const StyledCard = styled.div(() => ({
+  overflow: "hidden",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  cursor: "pointer",
+  transition: "transform 200ms ease-in",
+  width: "290px",
+  height: "646px",
+  backgroundColor: "white",
+  ":hover": {
+    transform: "scale(1.05)",
+  },
+}));
+
+const ImageContainer = styled.div`
+  width: 100%;
+`;
+
+const BackgroundImage = styled.img(() => ({
+  width: "100%",
+  height: "222px",
+}));
+
+const ProductImage = styled.img(() => ({
+  width: "100%",
+  marginTop: "-200px",
+}));
+
+const CardBody = styled.div(() => ({
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+}));
+
+const Title = styled.h2(() => ({
+  marginLeft: "20px",
+}));
+
+const SubTitle = styled.p(() => ({
+  marginLeft: "20px",
+  marginTop: "10px",
+}));
+
+const Info = styled.p(() => ({
+  marginLeft: "20px",
+}));
+
+const ButtonsContainer = styled.div(() => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const CartButton = styled.button(() => ({
+  width: "250px",
+  height: "60px",
+  borderRadius: "30px",
+  backgroundColor: "white",
+  marginBottom: "10px",
+  ":hover": {
+    backgroundColor: "black",
+    color: "white",
+    border: "none",
+  },
+}));
+
+const SubButton = styled.button(({ bgColor }) => ({
+  width: "250px",
+  height: "60px",
+  borderRadius: "30px",
+  backgroundColor: bgColor,
+  marginBottom: "20px",
+  border: "none",
+  fontWeight: 600,
+}));
+
+function Card({
+  title,
+  subTitle,
+  info,
+  backgroundImage,
+  productImage,
+  buttonColor,
+  regPrice,
+  subPrice,
+}) {
+  return (
+    <Container>
+      <StyledCard>
+        <ImageContainer>
+          <BackgroundImage src={backgroundImage}></BackgroundImage>
+          <ProductImage src={productImage}></ProductImage>
+        </ImageContainer>
+        <CardBody>
+          <Title>{title}</Title>
+          <SubTitle>{subTitle}</SubTitle>
+        </CardBody>
+        <Info>
+          {" "}
+          <b>Key ingredients: </b>
+          {info}
+        </Info>
+        <ButtonsContainer>
+          <CartButton>Add to Cart ━━━━ {regPrice}</CartButton>
+          <SubButton bgColor={buttonColor}>
+            ⓘ Subscribe ━━━━ {subPrice}
+          </SubButton>
+        </ButtonsContainer>
+      </StyledCard>
+    </Container>
+  );
+}
+
+export default Card;
